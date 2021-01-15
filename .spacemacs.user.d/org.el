d08f6306297cd7882543274d72192e3648b14acb
efilion <eric.l.filion@gmail.com>
2021-01-15 14:37:08 -0800

    Adding CANCELED todo state.

--- .spacemacs.user.d/org.el
+++ .spacemacs.user.d/org.el
@@ -1,10 +1,11 @@
 (setq org-todo-keywords
-      '((sequence "TODO" "NEXT" "|" "DONE" "WAITING")))
+      '((sequence "TODO" "NEXT" "|" "DONE" "WAITING" "CANCELED")))
 
 (setq org-refile-targets
       '(("~/org/notes.org" :maxlevel . 1)
         ("~/org/gtd.org" :maxlevel . 1)
         ("~/org/calendar.org" :maxlevel . 1)
-        ("~/org/someday.org" :maxlevel . 1)))
+        ("~/org/someday.org" :maxlevel . 1)
+        ("~/org/archive.org" :maxlevel . 1)))
 
 (setq org-agenda-files (list "~/org/notes.org" "~/org/gtd.org" "~/org/calendar.org"))
